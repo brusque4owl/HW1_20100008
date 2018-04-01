ccc2899d6971c1d25168df919450b46982d45d74
강현구 <brusque4owl@gmail.com>
2018-04-02 01:38:36 +0900

    sword 물체 추가 및 동선 테스트

--- HW1_20100008/HW1_20100008/HW1_20100008.cpp
+++ HW1_20100008/HW1_20100008/HW1_20100008.cpp
@@ -18,6 +18,7 @@ glm::mat4 ViewMatrix, ProjectionMatrix, ViewProjectionMatrix;
 #define BUFFER_OFFSET(offset) ((GLvoid *) (offset))
 
 #define LOC_VERTEX 0
+
 ///////////////////////////////////////////////////////////////////////////////////////
 /////////////////////     variables for objects          //////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -38,7 +39,7 @@ GLfloat fox_centerx = 0.0f, fox_centery = 0.0f;
 GLfloat airplane_centerx = -500.0f, airplane_centery = 0.0f;
 GLfloat house_centerx = -100.0f, house_centery = 0.0f;
 GLfloat car_centerx = 100.0f, car_centery = 0.0f;
-GLfloat sword_centerx = 0.0f, sword_centery = 0.0f;
+GLfloat sword_centerx = 300.0f, sword_centery = 0.0f;
 bool fox_crash = 0;	// 0 for not crash, 1 for crashed; it is used in display()
 unsigned int set_key = 0;	// 0 for left, 1 for right, 2 for down, 3 for up
 
@@ -46,7 +47,8 @@ unsigned int set_key = 0;	// 0 for left, 1 for right, 2 for down, 3 for up
 GLfloat airplane_deltax = -10.0, airplane_deltay = -10.0;
 GLfloat house_deltax = -10.0, house_deltay = 10.0;
 GLfloat car_deltax = 6.0, car_deltay = -4.0;
-GLfloat sword_deltax = 1.0, sword_deltay = 1.0;
+GLfloat sword_deltax = 7.0, sword_deltay = 3.0;
+
 int win_width = 0, win_height = 0;
 float centerx = 0.0f, centery = 0.0f, rotate_angle = 0.0f;
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -271,7 +273,7 @@ void draw_line(void) { // Draw line in its MC.
 
 /// start : originated from 3.1.2DObjects_GLSL
 ///////////////////////////////////////////////////////////////////////////////////
-/////////////// airplane
+/////////////// airplane : object number 0
 ///////////////////////////////////////////////////////////////////////////////////
 
 #define AIRPLANE_BIG_WING 0
@@ -365,7 +367,7 @@ void draw_airplane() { // Draw airplane in its MC.
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
-/////////////// house
+/////////////// house : ojbect number 1
 ///////////////////////////////////////////////////////////////////////////////////
 #define HOUSE_ROOF 0
 #define HOUSE_BODY 1
@@ -439,7 +441,7 @@ void draw_house() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
-///////////////  car 
+///////////////  car : object number 2
 ///////////////////////////////////////////////////////////////////////////////////
 #define CAR_BODY 0
 #define CAR_FRAME 1
@@ -524,6 +526,92 @@ void draw_car() {
 
 	glUniform3fv(loc_primitive_color, 1, car_color[CAR_RIGHT_WHEEL]);
 	glDrawArrays(GL_TRIANGLE_FAN, 24, 4);
+
+	glBindVertexArray(0);
+}
+
+/////////////////////////////////////////////////////////////////////////
+//////////// sword : object number 3
+/////////////////////////////////////////////////////////////////////////
+#define SWORD_BODY 0
+#define SWORD_BODY2 1
+#define SWORD_HEAD 2
+#define SWORD_HEAD2 3
+#define SWORD_IN 4
+#define SWORD_DOWN 5
+#define SWORD_BODY_IN 6
+
+GLfloat sword_body[4][2] = { { -6.0, 0.0 },{ -6.0, -4.0 },{ 6.0, -4.0 },{ 6.0, 0.0 } };
+GLfloat sword_body2[4][2] = { { -2.0, -4.0 },{ -2.0, -6.0 } ,{ 2.0, -6.0 },{ 2.0, -4.0 } };
+GLfloat sword_head[4][2] = { { -2.0, 0.0 },{ -2.0, 16.0 } ,{ 2.0, 16.0 },{ 2.0, 0.0 } };
+GLfloat sword_head2[3][2] = { { -2.0, 16.0 },{ 0.0, 19.46 } ,{ 2.0, 16.0 } };
+GLfloat sword_in[4][2] = { { -0.3, 0.7 },{ -0.3, 15.3 } ,{ 0.3, 15.3 },{ 0.3, 0.7 } };
+GLfloat sword_down[4][2] = { { -2.0, -6.0 } ,{ 2.0, -6.0 },{ 4.0, -8.0 },{ -4.0, -8.0 } };
+GLfloat sword_body_in[4][2] = { { 0.0, -1.0 } ,{ 1.0, -2.732 },{ 0.0, -4.464 },{ -1.0, -2.732 } };
+
+GLfloat sword_color[7][3] = {
+	{ 139 / 255.0f, 69 / 255.0f, 19 / 255.0f },
+	{ 139 / 255.0f, 69 / 255.0f, 19 / 255.0f },
+	{ 155 / 255.0f, 155 / 255.0f, 155 / 255.0f },
+	{ 155 / 255.0f, 155 / 255.0f, 155 / 255.0f },
+	{ 0 / 255.0f, 0 / 255.0f, 0 / 255.0f },
+	{ 139 / 255.0f, 69 / 255.0f, 19 / 255.0f },
+	{ 255 / 255.0f, 0 / 255.0f, 0 / 255.0f }
+};
+
+GLuint VBO_sword, VAO_sword;
+void prepare_sword() {
+	GLsizeiptr buffer_size = sizeof(sword_body) + sizeof(sword_body2) + sizeof(sword_head) + sizeof(sword_head2) + sizeof(sword_in) + sizeof(sword_down) + sizeof(sword_body_in);
+
+	// Initialize vertex buffer object.
+	glGenBuffers(1, &VBO_sword);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_sword);
+	glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STATIC_DRAW); // allocate buffer object memory
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(sword_body), sword_body);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body), sizeof(sword_body2), sword_body2);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body) + sizeof(sword_body2), sizeof(sword_head), sword_head);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body) + sizeof(sword_body2) + sizeof(sword_head), sizeof(sword_head2), sword_head2);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body) + sizeof(sword_body2) + sizeof(sword_head) + sizeof(sword_head2), sizeof(sword_in), sword_in);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body) + sizeof(sword_body2) + sizeof(sword_head) + sizeof(sword_head2) + sizeof(sword_in), sizeof(sword_down), sword_down);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(sword_body) + sizeof(sword_body2) + sizeof(sword_head) + sizeof(sword_head2) + sizeof(sword_in) + sizeof(sword_down), sizeof(sword_body_in), sword_body_in);
+
+	// Initialize vertex array object.
+	glGenVertexArrays(1, &VAO_sword);
+	glBindVertexArray(VAO_sword);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_sword);
+	glVertexAttribPointer(LOC_VERTEX, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void draw_sword() {
+	glBindVertexArray(VAO_sword);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_BODY]);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_BODY2]);
+	glDrawArrays(GL_TRIANGLE_FAN, 4, 4);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_HEAD]);
+	glDrawArrays(GL_TRIANGLE_FAN, 8, 4);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_HEAD2]);
+	glDrawArrays(GL_TRIANGLE_FAN, 12, 3);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_IN]);
+	glDrawArrays(GL_TRIANGLE_FAN, 15, 4);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_DOWN]);
+	glDrawArrays(GL_TRIANGLE_FAN, 19, 4);
+
+	glUniform3fv(loc_primitive_color, 1, sword_color[SWORD_BODY_IN]);
+	glDrawArrays(GL_TRIANGLE_FAN, 23, 4);
 
 	glBindVertexArray(0);
 }
@@ -1090,6 +1178,12 @@ void display(void) {
 	glUniformMatrix4fv(loc_ModelViewProjectionMatrix, 1, GL_FALSE, &ModelViewProjectionMatrix[0][0]);
 	draw_car();
 
+	ModelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(sword_centerx, sword_centery, 0.0f));
+	ModelMatrix = glm::scale(ModelMatrix, glm::vec3(2.0f, 2.0f, 1.0f));
+	ModelViewProjectionMatrix = ViewProjectionMatrix * ModelMatrix;
+	glUniformMatrix4fv(loc_ModelViewProjectionMatrix, 1, GL_FALSE, &ModelViewProjectionMatrix[0][0]);
+	draw_sword();
+
 	//////////////////DRAW_FOX BELOW//////////////////////////
 	ModelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(fox_centerx, fox_centery, 0.0f));
 	ModelMatrix = glm::scale(ModelMatrix, glm::vec3(2.0f, 2.0f, 1.0f));
@@ -1143,7 +1237,6 @@ void display(void) {
 	ModelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(fox_centerx, fox_centery, 0.0f));
 	ModelMatrix = glm::scale(ModelMatrix, glm::vec3(2.0f, 2.0f, 1.0f));
 	ModelMatrix = glm::rotate(ModelMatrix, rotate_angle, glm::vec3(0.0f, 0.0f, 1.0f));
-
 	ModelViewProjectionMatrix = ViewProjectionMatrix * ModelMatrix;
 	glUniformMatrix4fv(loc_ModelViewProjectionMatrix, 1, GL_FALSE, &ModelViewProjectionMatrix[0][0]);
 	draw_hat();
@@ -1297,6 +1390,7 @@ void prepare_scene(void) {
 	prepare_airplane();
 	prepare_house();
 	prepare_car();
+	prepare_sword();
 	prepare_hat();
 	prepare_fox_fixed();
 	prepare_fox_arm_1();
